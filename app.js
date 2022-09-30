cf788dc212de6c1ae493d4f144a1cb2bae836eec
My name <sindanim@gmail.com>
2022-09-30 17:00:02 +0200

    modified:   app.js 	renamed:    images/m.jpeg -> images/menu-item.jpeg 	new file:   images/rachealnafulaspaenicoverletter.pdf 	modified:   index.html

--- app.js
+++ app.js
@@ -71,6 +71,14 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ]
 
 const sectionCenter = document.querySelector(".section-center")
@@ -80,54 +88,18 @@ const container = document.querySelector(".btn-container")
 window.addEventListener("DOMContentLoaded", function () {
   displayMenuItems(menu)
   displayMenuButtons()
-  // find unique categories
-  const categories = menu.reduce(
-    function (values, item) {
-      if (!values.includes(item.category)) {
-        values.push(item.category)
-      }
-
-      return values
-    },
-    ["all"]
-  )
-  //iterate over categories and return buttons
-  const categoryBtns = categories
-    .map(function (category) {
-      return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>  
-        </button>`
-    })
-    .join("")
-  container.innerHTML = categoryBtns
-  const filterBtns = document.querySelectorAll(".filter-btn")
-  //filter items
-  filterBtns.forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
-      const category = e.currentTarget.dataset.id
-      const menuCategory = menu.filter(function (menuItem) {
-        //console.log(menuItem.category)
-        if (menuItem.category === category) {
-          return menuItem
-        }
-      })
-      //console.log(menuCategory)
-      if (category == "all") {
-        displayMenuItems(menu)
-      } else {
-        displayMenuItems(menuCategory)
-      }
-    })
-  })
 })
+//use map to return all the categories
 
+//look for array
 function displayMenuItems(menuItems) {
+  //iterate over the array instead of menu
   let displayMenu = menuItems.map(function (item) {
-    console.log(item)
     return `<article class="menu-item">
           <img
-            src=${item.img}
+            src="${item.img}"
             class="photo"
-            alt=${item.title}
+            alt="${item.title}"
           />
           <div class="item-info">
             <header>
@@ -145,19 +117,22 @@ function displayMenuItems(menuItems) {
 }
 function displayMenuButtons() {
   const categories = menu.reduce(
-    function (values, item) {
-      if (!values.includes(item.category)) {
-        values.push(item.category)
+    function (acc, crr) {
+      if (!acc.includes(crr.category)) {
+        acc.push(crr.category)
       }
-
-      return values
+      return acc
     },
     ["all"]
   )
-  //iterate over categories and return buttons
   const categoryBtns = categories
     .map(function (category) {
-      return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>  
+      return `<button
+       class="filter-btn"
+          type="button"
+          data-id="${category}"
+        >
+          ${category}
         </button>`
     })
     .join("")
@@ -166,15 +141,17 @@ function displayMenuButtons() {
   //filter items
   filterBtns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
+      // assign the id to a variable
       const category = e.currentTarget.dataset.id
+      //filter out array create new array
       const menuCategory = menu.filter(function (menuItem) {
         //console.log(menuItem.category)
         if (menuItem.category === category) {
           return menuItem
         }
       })
-      //console.log(menuCategory)
-      if (category == "all") {
+      //console.log(menuCategory);
+      if (category === "all") {
         displayMenuItems(menu)
       } else {
         displayMenuItems(menuCategory)
